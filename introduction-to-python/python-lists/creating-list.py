7539f39464f823004bc6b7681f641efa1a858e26
Ralf <ralfferreira995@gmail.com>
2023-01-27 06:51:56 -0300

    Modify the exercise to create a list with different types

--- introduction-to-python/python-lists/creating-list.py
+++ introduction-to-python/python-lists/creating-list.py
@@ -5,8 +5,8 @@ liv = 20.0
 bed = 10.75
 bath = 9.50
 
-# Create list areas
-areas = [hall, kit, liv, bed, bath]
+# Adapt list areas
+areas = ["hallway", hall, "kitchen", kit, "living room", liv, "bedroom", bed, "bathroom", bath]
 
 # Print areas
 print(areas)
