023a70939f35ece6e11566370f5827a357ecbcb2
Ralf <ralfferreira995@gmail.com>
2023-01-27 06:35:01 -0300

    Modify variable assignment exercise

--- introduction-to-python/python-basics/variable-assignment.py
+++ introduction-to-python/python-basics/variable-assignment.py
@@ -1,5 +1,11 @@
 # Create a variable savings
 savings = 100
 
-# Print out savings
-print(savings)
+# Create a variable growth_multiplier
+growth_multiplier = 1.1
+
+# Calculate result
+result = savings * growth_multiplier ** 7
+
+# Print out result
+print(result)
