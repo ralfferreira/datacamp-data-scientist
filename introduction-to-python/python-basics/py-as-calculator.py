7ea9483ea89151c0c5405d7f7a6a6c7457246334
Ralf <ralfferreira995@gmail.com>
2023-01-26 22:24:35 -0300

    Add some exercises of Python Basics

---
+++ introduction-to-python/python-basics/py-as-calculator.py
@@ -0,0 +1,16 @@
+# Addition, subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication, division, modulo, and exponentiation
+print(3 * 5)
+print(10 / 2)
+print(18 % 7)
+print(4 ** 2)
+
+# How much is your $100 worth after 7 years?
+print(100 * 1.1 ** 7)
+
+# Suppose you have $100, which you can invest with a 10% return each year. 
+# After one year, it's  dollars, and after two years it's . 
+# Add code to calculate how much money you end up with after 7 years, and print the result.
