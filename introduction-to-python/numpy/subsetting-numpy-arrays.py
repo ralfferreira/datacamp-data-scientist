dbcab79ed2caaaf71ea08211f3f7ccf4cb0045d8
Ralf <ralfferreira995@gmail.com>
2023-02-01 14:04:12 -0300

    Modify subsetting numpy arrays exercise

--- introduction-to-python/numpy/subsetting-numpy-arrays.py
+++ introduction-to-python/numpy/subsetting-numpy-arrays.py
@@ -11,4 +11,4 @@ np_height_in = np.array(height_in)
 print(np_weight_lb[50])
 
 # Print out sub-array of np_height_in: index 100 up to and including index 110
-print(np_height_in[100:110])
+print(np_height_in[100:111])
